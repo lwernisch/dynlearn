6d217d2520b421029dfbcb21f45ae52717a6b141
JohnReid <johnbaronreid@netscape.net>
2019-02-01 10:36:19 +0000

    Add URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dynlearn',
     version='0.1.0',
-    # url='https://github.com/mypackage.git',
+    url='https://lwernisch.github.io/dynlearn/',
     author='Lorenz Wernisch',
     author_email='lwernisch@gmail.com',
     description='Dynamical system active learning with Gaussian processes',
